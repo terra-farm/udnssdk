de72beaeffda09eff9c377bfddf220e306683d44
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2016-03-07 00:08:50 -0800

    account.go: extract AccountKey type

--- account.go
+++ account.go
@@ -27,18 +27,52 @@ type AccountListDTO struct {
 
 // accountPath links to the account url.
 func accountPath(accountName string) string {
-	path := "accounts"
-	if accountName != "" {
-		path = fmt.Sprintf("accounts/%s", accountName)
-	}
-	return path
+	return AccountKey(accountName).URI()
 }
 
 // GetAccountsOfUser gets all the accounts of user
 func (s *AccountsService) GetAccountsOfUser() ([]Account, *Response, error) {
+	return s.Select()
+}
+
+// GetAccountStatus Get the status of a account.
+func (s *AccountsService) GetAccountStatus(tid string) (Account, *Response, error) {
+	return s.Find(AccountKey(tid))
+}
+
+// ListAccounts lists accounts
+func (s *AccountsService) ListAccounts(query string, offset, limit int) ([]Account, *Response, error) {
+	return s.Select()
+}
+
+// DeleteAccount deletes a account.
+func (s *AccountsService) DeleteAccount(tid string) (*Response, error) {
+	return s.Delete(AccountKey(tid))
+}
+
+// ======== //
+
+// AccountKey represents the string identifier of an Account
+type AccountKey string
+
+// URI generates the URI for an Account
+func (a AccountKey) URI() string {
+	uri := "accounts"
+	if a != "" {
+		uri = fmt.Sprintf("accounts/%s", a)
+	}
+	return uri
+}
+
+// AccountsURI generates the URI for Accounts collection
+func AccountsURI() string {
+	return "accounts"
+}
+
+// Select requests all Accounts of user
+func (s *AccountsService) Select() ([]Account, *Response, error) {
 	var ald AccountListDTO
-	uri := accountPath("")
-	res, err := s.client.get(uri, &ald)
+	res, err := s.client.get(AccountsURI(), &ald)
 
 	accts := []Account{}
 	for _, t := range ald.Accounts {
@@ -47,74 +81,14 @@ func (s *AccountsService) GetAccountsOfUser() ([]Account, *Response, error) {
 	return accts, res, err
 }
 
-/*
-// TODO:  Implement Zones
-func (s *AccountsService) GetZonesOfAccount(accountName string) ([]Account, *Response, error) {
-	reqStr := fmt.Sprintf("%s/zones", accountPath(accountName))
-	var ald AccountListDTO
-	log.Printf("In GetZonesOfAccount(%s)..  ReqStr: %s\n", accountName, reqStr)
-	res, err := s.client.get(reqStr, &ald)
-	if err != nil {
-		return []Account{}, res, err
-	}
-	accts := []Account{}
-	for _, t := range ald.Accounts {
-		accts = append(accts, t)
-	}
-	log.Printf("Exiting GetZonesOfAccount(%s)..\n", accountName)
-	return accts, res, nil
-}
-*/
-
-// GetAccountStatus Get the status of a account.
-func (s *AccountsService) GetAccountStatus(tid string) (Account, *Response, error) {
-	reqStr := accountPath(tid)
+// Find requests an Account by AccountKey
+func (s *AccountsService) Find(a AccountKey) (Account, *Response, error) {
 	var t Account
-	res, err := s.client.get(reqStr, &t)
-	if err != nil {
-		return t, res, err
-	}
+	res, err := s.client.get(a.URI(), &t)
 	return t, res, err
 }
 
-// GetAccountResultByURI gets account result by URI
-func (s *AccountsService) GetAccountResultByURI(uri string) (*Response, error) {
-	req, err := s.client.NewRequest("GET", uri, nil)
-	if err != nil {
-		return nil, err
-	}
-	res, err := s.client.HTTPClient.Do(req)
-
-	if err != nil {
-		return &Response{Response: res}, err
-	}
-	return &Response{Response: res}, err
-}
-
-// ListAccounts lists accounts
-func (s *AccountsService) ListAccounts(query string, offset, limit int) ([]Account, *Response, error) {
-	// TODO: Soooo... This function does not handle pagination of Accounts....
-	//v := url.Values{}
-
-	reqStr := "accounts"
-	var tld AccountListDTO
-	//wrappedAccounts := []Account{}
-
-	res, err := s.client.get(reqStr, &tld)
-	if err != nil {
-		return []Account{}, res, err
-	}
-
-	accounts := []Account{}
-	for _, t := range tld.Accounts {
-		accounts = append(accounts, t)
-	}
-
-	return accounts, res, nil
-}
-
-// DeleteAccount deletes a account.
-func (s *AccountsService) DeleteAccount(tid string) (*Response, error) {
-	path := accountPath(tid)
-	return s.client.delete(path, nil)
+// Delete requests deletion of an Account by AccountKey
+func (s *AccountsService) Delete(a AccountKey) (*Response, error) {
+	return s.client.delete(a.URI(), nil)
 }
